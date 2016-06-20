e0b2587c5c8f879713d3fad3b1163120e0c7219e
daikwn <dai-kawano@dac.co.jp>
2016-06-20 05:16:27 +0000

    sdhshshhs

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+  config.access_token = "EAANOsJYlLuABAKvqgEX0zbPjsnDZBdigZANwtZCjg22lAZCSGZAWgGOA9FgxyjOskW9vHrBvfKxKPfWYrmnrroaNi6VjObPgwY5WuSo4BEKkfpccaLuZAWZAstaHDmxmrZArcU1rAKg4M09Ug75aR3i8sUtECz54oQqekyr1q5NNBIWXVvS24Mux"
+  config.validation_token = "fractal"
+  config.secret_token = "1ce8e4db32a8419a23e72c0fe59e0de1"
+end
