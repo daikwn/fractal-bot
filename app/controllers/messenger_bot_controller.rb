6631146ece3f1b3fd109d5b3a05a503151e2730c
daikwn <dai-kawano@dac.co.jp>
2016-06-20 05:35:45 +0000

    sdhshshhs

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,7 +1,11 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
+    profile = sender.get_profile[:body]
+    profile_last_name = profile['last_name']
+    profile_first_name = profile['first_name']
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     sender.reply({ text: "Reply: #{event['message']['text']}" })
+    sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんこんにちは" })
   end
 
   def delivery(event, sender)
