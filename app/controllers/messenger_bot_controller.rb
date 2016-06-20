303194ce906f5dd046a7794e43438a1194ce362b
daikwn <dai-kawano@dac.co.jp>
2016-06-20 06:39:32 +0000

    dhjbd

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,10 +1,21 @@
 class MessengerBotController < ActionController::Base
+    require 'yahoo_parse_api'
+    @application_ID = "dj0zaiZpPXZhTWlrcHFVME9xOCZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Y-"
+    
   def message(event, sender)
+    YahooParseApi::Config.app_id = @application_ID
+    parse_api = YahooParseApi::Parse.new
+    # GET Request
+    result = parse_api.parse(['message']['text'], {
+            results: 'ma,uniq',
+            uniq_filter: '9|10'
+         })
+         
     profile = sender.get_profile[:body]
     profile_last_name = profile['last_name']
     profile_first_name = profile['first_name']
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-    sender.reply({ text: "Reply: #{event['message']['text']}" })
+    sender.reply({ text: "Reply: #{results}" })
     sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんこんにちは" })
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 mount Messenger::Bot::Space => "/webhook"
+
 end
+
+
+
