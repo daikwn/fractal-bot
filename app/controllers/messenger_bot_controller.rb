ad9922baba38cf3438dae7d7985a424fd84d8a13
daikwn <dai-kawano@dac.co.jp>
2016-06-21 07:23:37 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -13,15 +13,40 @@ class MessengerBotController < ActionController::Base
              results: 'ma,uniq',
              uniq_filter: '9|10'})
     bot_rep = result['ResultSet']['ma_result']['word_list']['word']
+    
     meishi = bot_rep.select do |b|
       b["pos"] == "名詞"
     end
     
+    doushi = bot_rep.select do |b|
+      b["pos"] == "動詞"
+    end
+    
+    joshi = bot_rep.select do |b|
+      b["pos"] == "助詞"
+    end
+    
+    jodoushi = bot_rep.select do |b|
+      b["pos"] == "助動詞"
+    end
+    
+    spword = bot_rep.select do |b|
+      b["pos"] == "特殊"
+    end
+    
+    
     rep_m = meishi.count
+    rep_d = doushi.count
+    rep_j = joshi.count
+    rep_jd = jodoushi.count
+    rep_sp = spword.count
 
-             
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     sender.reply({ text: "Reply: #{rep_m}" })
+    sender.reply({ text: "Reply: #{rep_d}" })
+    sender.reply({ text: "Reply: #{rep_j}" })
+    sender.reply({ text: "Reply: #{rep_jd}" })
+    sender.reply({ text: "Reply: #{rep_sp}" })
 
     sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんこんにちは" })
   end
