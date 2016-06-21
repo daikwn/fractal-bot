8a0c890ae47299d0c75cda19f1cfbc86ed76e91a
daikwn <dai-kawano@dac.co.jp>
2016-06-21 03:02:05 +0000

    ha

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -16,16 +16,17 @@ class MessengerBotController < ActionController::Base
     rep_s = bot_rep.count("名詞")
     rep_v = bot_rep.count("動詞")
     rep_c = bot_rep.count("助詞")
+    rep_cv = bot_rep.count("助動詞")
     rep_sp = bot_rep.count("特殊")
              
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     sender.reply({ text: "Reply: #{rep_s}" })
     sender.reply({ text: "Reply: #{rep_v}" })
     sender.reply({ text: "Reply: #{rep_c}" })
+    sender.reply({ text: "Reply: #{rep_cv}" })
     sender.reply({ text: "Reply: #{rep_sp}" })
     sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんこんにちは" })
   end
-  
 
   def delivery(event, sender)
   end
