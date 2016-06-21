aa24fb16a6e8b7d46d7914c9ddf65838dcb1bee7
daikwn <dai-kawano@dac.co.jp>
2016-06-21 08:54:56 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,47 +1,40 @@
 require 'yahoo_parse_api'
 
 class MessengerBotController < ActionController::Base
-  @cashe = 0
-    
   def message(event, sender)
     YahooParseApi::Config.app_id = 'dj0zaiZpPXZhTWlrcHFVME9xOCZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Y-'
     parse_api = YahooParseApi::Parse.new
-    
-    if event['message']['text'] == 'フラクタル'
-       @cashe = 1
-       sender.reply({ text: "あなたの自己紹介を30文字以上で記入してください。" })
-      elsif cashe == 1
-      result = parse_api.parse(event['message']['text'], {
+
+       result = parse_api.parse(event['message']['text'], {
                results: 'ma,uniq',
                uniq_filter: '9|10'})
-      bot_rep = result['ResultSet']['ma_result']['word_list']['word']
-      meishi = bot_rep.select do |b|
+       bot_rep = result['ResultSet']['ma_result']['word_list']['word']
+       meishi = bot_rep.select do |b|
         b["pos"] == "名詞"
-      end
-      doushi = bot_rep.select do |b|
+       end
+       doushi = bot_rep.select do |b|
         b["pos"] == "動詞"
-      end
-      joshi = bot_rep.select do |b|
+       end
+       joshi = bot_rep.select do |b|
         b["pos"] == "助詞"
-      end
-      jodoushi = bot_rep.select do |b|
+       end
+       jodoushi = bot_rep.select do |b|
         b["pos"] == "助動詞"
-      end
-      spword = bot_rep.select do |b|
+       end
+       spword = bot_rep.select do |b|
         b["pos"] == "特殊"
-      end
-      rep_m = meishi.count
-      rep_d = doushi.count
-      rep_j = joshi.count
-      rep_jd = jodoushi.count
-      rep_sp = spword.count
-      sender.reply({ text: "Reply: #{rep_m}" })
-      sender.reply({ text: "Reply: #{rep_d}" })
-      sender.reply({ text: "Reply: #{rep_j}" })
-      sender.reply({ text: "Reply: #{rep_jd}" })
-      sender.reply({ text: "Reply: #{rep_sp}" })
-    end
-  
+       end
+       rep_m = meishi.count
+       rep_d = doushi.count
+       rep_j = joshi.count
+       rep_jd = jodoushi.count
+       rep_sp = spword.count
+       sender.reply({ text: "Reply: #{rep_m}" })
+       sender.reply({ text: "Reply: #{rep_d}" })
+       sender.reply({ text: "Reply: #{rep_j}" })
+       sender.reply({ text: "Reply: #{rep_jd}" })
+       sender.reply({ text: "Reply: #{rep_sp}" })
+   
   end
 
   def delivery(event, sender)
