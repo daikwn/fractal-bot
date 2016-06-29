7c3e63ba987a0e9fe37d0084c8521f3332720e0c
daikwn <dai-kawano@dac.co.jp>
2016-06-29 08:02:38 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -3,118 +3,81 @@ require 'yahoo_parse_api'
 class MessengerBotController < ActionController::Base
   
   def message(event, sender)
+    YahooParseApi::Config.app_id = 'dj0zaiZpPXZhTWlrcHFVME9xOCZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Y-'
+    parse_api = YahooParseApi::Parse.new
+    profile = sender.get_profile[:body] # default field [locale, timezone, gender, first_name, last_name, profile_pic]
     text = event['message']['text']
+    profile = sender.get_profile[:body]
+    profile_last_name = profile['last_name']
+    profile_first_name = profile['first_name']
+    result = parse_api.parse(text, {
+             results: 'ma,uniq',
+             uniq_filter: '1|2'})
+    bot_rep = result['ResultSet']['ma_result']['word_list']['word']
     
-    if text.end_with?("起動") 
-       sender.reply({ "attachment":{
-            "type":"template",
-            "payload":{
-                "template_type":"button",
-                "text":"川端康成日本語きれいさ判定BOTへようこそ！",
-                "buttons":[
-                    {
-                        "type":"postback",
-                        "title":"使ってみる。",
-                        "payload":"OVER"
-                    },
-                    {
-                        "type":"postback",
-                        "title":"興味ない。",
-                        "payload":"UNDER"
-                    }
-                ]
-            }
-          }
-        })
-      else
-        sender.reply({ text: "【起動】で動きます。" })
+    meishi = bot_rep.select do |b|
+      b["pos"] == "名詞"
+    end
+    doushi = bot_rep.select do |b|
+      b["pos"] == "動詞"
+    end
+    joshi = bot_rep.select do |b|
+      b["pos"] == "助詞"
+    end
+    jodoushi = bot_rep.select do |b|
+      b["pos"] == "助動詞"
+    end
+    hukushi = bot_rep.select do |b|
+      b["pos"] == "副詞"
+    end
+    keiyoushi = bot_rep.select do |b|
+      b["pos"] == "形容詞"
+    end
+
+    rep_m = meishi.count
+    rep_d = doushi.count
+    rep_j = joshi.count
+    rep_jd = jodoushi.count
+    rep_hk = hukushi.count
+    rep_ky = keiyoushi.count
+    
+    usercountTO = rep_m + rep_d + rep_j + rep_jd + rep_hk + rep_ky
+    userSL_m = (0.3229 - rep_m/usercountTO.to_f).abs
+    userSL_d = (0.1189 - rep_d/usercountTO.to_f).abs
+    userSL_j = (0.4164 - rep_j/usercountTO.to_f).abs
+    userSL_jd = (0.1416 - rep_jd/usercountTO.to_f).abs
+    userSL_hk = (0.0226 - rep_hk/usercountTO.to_f).abs
+    userSL_ky = (0.0283 - rep_ky/usercountTO.to_f).abs
+    userslTO = userSL_m + userSL_d + userSL_j + userSL_jd + userSL_hk + userSL_ky
+    
+    score = 100*(1-userslTO.to_f)
+    
+    sender.reply({ text: "名詞の数: #{rep_m}" })
+    sender.reply({ text: "動詞の数: #{rep_d}" })
+    sender.reply({ text: "助詞の数: #{rep_j}" })
+    sender.reply({ text: "助動詞の数: #{rep_jd}" })
+    sender.reply({ text: "副詞の数: #{rep_hk}" })
+    sender.reply({ text: "形容詞の数: #{rep_ky}" })
+    sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
+     
+    if  0 < score < 20 then
+      sender.reply({ text: "論外ですね" })
+    elsif 21 < score < 50 then
+      sender.reply({ text: "ちょっと文章が少なすぎるのではないでしょうか？もう少し文章を長くしてみるいいかもしれませんね。" })
+    elsif 51 < score < 80 then
+      sender.reply({ text: "いい感じです。もう少しいろいろな表現をしてみると更にスコアがあがる…かもしれません" })
+    elsif 81 < score < 99 then
+      sender.reply({ text: "もはやあなたが川端康成です。" })
+    else
+      sender.reply({ text: "ﾋﾞｭｰﾃｨﾌｫｰ" })
     end
   end
   def delivery(event, sender)
-    text = event['message']['text']
-      YahooParseApi::Config.app_id = 'dj0zaiZpPXZhTWlrcHFVME9xOCZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Y-'
-      parse_api = YahooParseApi::Parse.new
-      profile = sender.get_profile[:body] # default field [locale, timezone, gender, first_name, last_name, profile_pic]
-      profile_last_name = profile['last_name']
-      profile_first_name = profile['first_name']
-      result = parse_api.parse(text, {
-                               results: 'ma,uniq',
-                               uniq_filter: '1|2'})
-      bot_rep = result['ResultSet']['ma_result']['word_list']['word']
-    
-      meishi = bot_rep.select do |b|
-        b["pos"] == "名詞"
-      end
-      doushi = bot_rep.select do |b|
-        b["pos"] == "動詞"
-      end
-      joshi = bot_rep.select do |b|
-        b["pos"] == "助詞"
-      end
-      jodoushi = bot_rep.select do |b|
-        b["pos"] == "助動詞"
-      end
-      hukushi = bot_rep.select do |b|
-        b["pos"] == "副詞"
-      end
-      keiyoushi = bot_rep.select do |b|
-        b["pos"] == "形容詞"
-      end
-
-      rep_m = meishi.count
-      rep_d = doushi.count
-      rep_j = joshi.count
-      rep_jd = jodoushi.count
-      rep_hk = hukushi.count
-      rep_ky = keiyoushi.count
-    
-      usercountTO = rep_m + rep_d + rep_j + rep_jd + rep_hk + rep_ky
-      userSL_m = (0.3229 - rep_m/usercountTO.to_f).abs
-      userSL_d = (0.1189 - rep_d/usercountTO.to_f).abs
-      userSL_j = (0.4164 - rep_j/usercountTO.to_f).abs
-      userSL_jd = (0.1416 - rep_jd/usercountTO.to_f).abs
-      userSL_hk = (0.0226 - rep_hk/usercountTO.to_f).abs
-      userSL_ky = (0.0283 - rep_ky/usercountTO.to_f).abs
-      userslTO = userSL_m + userSL_d + userSL_j + userSL_jd + userSL_hk + userSL_ky
-    
-      score = 100*(1-userslTO.to_f)
-    
-      sender.reply({ text: "名詞の数: #{rep_m}" })
-      sender.reply({ text: "動詞の数: #{rep_d}" })
-      sender.reply({ text: "助詞の数: #{rep_j}" })
-      sender.reply({ text: "助動詞の数: #{rep_jd}" })
-      sender.reply({ text: "副詞の数: #{rep_hk}" })
-      sender.reply({ text: "形容詞の数: #{rep_ky}" })
-      sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-     
-      if  0 < score < 20 
-        sender.reply({ text: "論外ですね" })
-      elsif 21 < score < 50 
-        sender.reply({ text: "ちょっと文章が少なすぎるのではないでしょうか？もう少し文章を長くしてみるいいかもしれませんね。" })
-      elsif 51 < score < 80 
-        sender.reply({ text: "いい感じです。もう少しいろいろな表現をしてみると更にスコアがあがる…かもしれません" })
-      elsif 81 < score < 99 
-        sender.reply({ text: "もはやあなたが川端康成です。" })
-      else
-        sender.reply({ text: "ﾋﾞｭｰﾃｨﾌｫｰ" })
-      end
-    
   end
-  
   def postback(event, sender)
     payload = event["postback"]["payload"]
-    
     case payload
-    when "OVER","UNDER"
-    
-    if payload == "OVER"
-      sender.reply({ text: "一文か二文程度で短文を入力してください。" })
-      delivery(message,sender)
-      
-    elsif postback == "UNDER"
-      sender.reply({ text: "そっかぁ…" })
-    end
-    
+    when :something
     end
   end
 end
