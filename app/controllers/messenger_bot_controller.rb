de3b47b928647a95a5005c4dc94f5b0348523e1c
daikwn <dai-kawano@dac.co.jp>
2016-06-29 06:48:49 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -58,7 +58,7 @@ class MessengerBotController < ActionController::Base
     sender.reply({ text: "助動詞: #{rep_jd}" })
     sender.reply({ text: "副詞: #{rep_hk}" })
     sender.reply({ text: "形容詞: #{rep_ky}" })
-    sender.reply({ text: "あなたの得点: #{score}" })
+    sender.reply({ text: "あなたの得点: #{score.ceil}" })
     sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんこんにちは" })
     
   end
