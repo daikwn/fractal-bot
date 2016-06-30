83157daef690479b3d8ea8d6040ce1f74a2e7084
daikwn <dai-kawano@dac.co.jp>
2016-06-30 06:19:59 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -92,8 +92,19 @@ def message(event, sender)
     
     @@key = 0
     
-    sender.reply({ text: "0点です。"})
-    
+    if 0 < score && score <= 20
+      sender.reply({ text: "★やる気あんのか"})
+    elsif 20 < score && score <= 40
+      sender.reply({ text: "★少し文章が短すぎるかもしれませんね。もう一文増やしてみればよいのでは？"})
+    elsif 40 < score && score <= 60
+      sender.reply({ text: "★うーん…表現に工夫を加えてみましょう。比喩表現を入れてみるとか？"})
+    elsif 60 < score && score <= 80
+      sender.reply({ text: "★なかなかハイスコアです。ちなみに川端康成の文章をテストすると平均85点くらいです。"})
+    elsif 80 < score && score <= 100
+      sender.reply({ text: "★ﾋﾞｭｰﾃｨﾌｫｰ"})
+    elsif score <= 0
+      sender.reply({ text: "文章になってません。"})
+    end
   end
 end
   
