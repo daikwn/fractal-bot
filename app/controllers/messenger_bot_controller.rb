d971cb7a0690ba157b8302f4519a135c479633a8
daikwn <dai-kawano@dac.co.jp>
2016-06-30 05:57:01 +0000

    s

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -84,26 +84,27 @@ def message(event, sender)
     sender.reply({ text: "副詞の数: #{rep_hk}" })
     sender.reply({ text: "形容詞の数: #{rep_ky}" })
     
-     if 0 < score <= 20
+    @@key = 0
+    
+    if 0 < score
       sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-      sender.reply({text: "★やる気あんのか"})
-     elsif 20 < score <= 40
-      sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-      sender.reply({text: "★少し文章が短すぎるかもしれませんね。もう一文増やしてみればよいのでは？"})
-     elsif 40 < score <= 60
-      sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-      sender.reply({text: "★うーん…表現に工夫を加えてみましょう。比喩表現を入れてみるとか？"})
-     elsif 60 < score <= 80
-      sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-      sender.reply({text: "★なかなかハイスコアです。ちなみに川端康成の文章をテストすると平均85点くらいです。"})
-     elsif 80 < score <= 100
-      sender.reply({ text: "#{profile_last_name} #{profile_first_name}さんの得点: #{score.ceil}" })
-      sender.reply({text: "★ﾋﾞｭｰﾃｨﾌｫｰ"})
-     elsif score <= 0
-      sender.reply({text: "0点です。"})
-     end
-     
-     @@key = 0
+    else
+      sender.reply({ text: "0点です。"})
+    end
+      
+    if 0 < score <= 20
+      sender.reply({ text: "★やる気あんのか"})
+    elsif 20 < score <= 40
+      sender.reply({ text: "★少し文章が短すぎるかもしれませんね。もう一文増やしてみればよいのでは？"})
+    elsif 40 < score <= 60
+      sender.reply({ text: "★うーん…表現に工夫を加えてみましょう。比喩表現を入れてみるとか？"})
+    elsif 60 < score <= 80
+      sender.reply({ text: "★なかなかハイスコアです。ちなみに川端康成の文章をテストすると平均85点くらいです。"})
+    elsif 80 < score <= 100
+      sender.reply({ text: "★ﾋﾞｭｰﾃｨﾌｫｰ"})
+    elsif score <= 0
+      sender.reply({ text: "文章になってません。"})
+    end
   end
 end
   
